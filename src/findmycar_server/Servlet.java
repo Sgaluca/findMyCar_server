f21a6c9711becf7e89e8a19f60abf6bb2d088223
Sgaluca <s>
2020-02-07 10:52:43 +0100

    Boh ho fatto cose

--- src/findmycar_server/Servlet.java
+++ src/findmycar_server/Servlet.java
@@ -4,12 +4,22 @@
  * and open the template in the editor.
  */
 package findmycar_server;
-import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 /**
  *
  * @author sgalu
  */
+@Path("/findmycar")
+@ApplicationPath("/resurces")
 public class Servlet extends HttpServlet{
     
+    @GET
+    @Path("/sayHello")
+    public String getHelloWordMsg(){
+        return "Hello Word";
+        
+    }
 }
